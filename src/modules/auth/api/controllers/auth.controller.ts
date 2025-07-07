6a4a81acc4554bfe6e3d61ea0a74aaf2b14a0a36
Erikles Geyzon <geyzon100@gmail.com>
2025-07-07 17:37:37 -0300

    feat: alter cookie SameSite attribute from 'strict' to 'lax' for login response

--- src/modules/auth/api/controllers/auth.controller.ts
+++ src/modules/auth/api/controllers/auth.controller.ts
@@ -40,7 +40,7 @@ export class AuthController {
         .cookie("auth_token", token, {
           httpOnly: true,
           secure: process.env.NODE_ENV === "production",
-          sameSite: "strict",
+          sameSite: "lax",
           maxAge: parseInt(process.env.JWT_EXPIRATION_TIME || "3600") * 1000,
         })
         .status(200)
